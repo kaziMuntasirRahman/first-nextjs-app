297689d5357e235f7e2b0da46d152b27e4758270
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2025-04-03 19:06:20 +0600

    issue fixed

--- src/app/(first project routes)/blogs/[slug]/page.jsx
+++ src/app/(first project routes)/blogs/[slug]/page.jsx
@@ -1,6 +1,8 @@
+import PageHead from "@/components/PageHead"
+
 const getBlogs = async (slug) => {
   // const response = await fetch('../../../../../public/data/blogs.json')
-  const response = await fetch('public/data/blogs.json')
+  const response = await fetch('https://raw.githubusercontent.com/kaziMuntasirRahman/first-nextjs-app/refs/heads/master/public/data/blogs.json')
   const data = await response.json()
   console.log(data)
   return data.posts.find(blog => blog.slug === slug)
@@ -8,12 +10,14 @@ const getBlogs = async (slug) => {
 
 const BlogDetails = async ({ params }) => {
   // console.log(params.slug)
-  const {title} = await getBlogs(params.slug)
-
+  const { title, content, author } = await getBlogs(params.slug)
   return (
     <div>
-      {/* <PageHead title={title} /> */}
-
+      <PageHead title={title} />
+      <section className="max-w-3xl min-h-[300px] border mx-auto p-4 flex flex-col gap-2">
+        <p className="text-lg text-gray-700">by <span className="font-semibold ">{author}</span></p>
+        <p>{content}</p>
+      </section>
     </div>
   );
 };
